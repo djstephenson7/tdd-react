464b9e049e8e543ab0c279aab0cd91edaae593a0
djstephenson7 <dan_0502@hotmail.co.uk>
2019-01-02 17:08:53 +0000

    Introduced products into App.js

--- react-tdd/src/App.js
+++ react-tdd/src/App.js
@@ -5,11 +5,23 @@ import './App.css';
 import ProductList from './components/ProductList'
 
 class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        products: [
+          { id: 1, name: 'AirMax 90', brand: 'Nike' },
+          { id: 2, name: 'Yeezy', brand: 'Adidas' },
+          { id: 3, name: 'Brogues', brand: 'Clarks' }
+        ]
+      }
+    }
+
   render() {
     return (
       <div>
         <h1>My Product Shop</h1>
-        <ProductList/>
+        <ProductList products={this.state.products}/>
       </div>
     );
   }
