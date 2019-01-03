f4bcbdb8623f44426e725450847c2beb578f8ead
djstephenson7 <dan_0502@hotmail.co.uk>
2019-01-03 17:00:49 +0000

    Tested for object name

--- react-tdd/src/components/ProductList.test.js
+++ react-tdd/src/components/ProductList.test.js
@@ -12,4 +12,16 @@ it('should render a list of components as an unordered list', () => {
 
   const wrapper = shallow(<ProductList products={mockProducts}/>)
   expect(wrapper.find('li').length).toEqual(mockProducts.length)
-})
+});
+
+it('should display the product name in each <li> element', () => {
+  const mockProducts  = [
+    { id: 1, name: 'MockProduct1', brand: 'MockBrand1' },
+    { id: 1, name: 'MockProduct2', brand: 'MockBrand2' },
+    { id: 1, name: 'MockProduct3', brand: 'MockBrand3' }
+  ]
+
+  const wrapper = shallow(<ProductList products={mockProducts}/>)
+  const firstElement = wrapper.find('li').first();
+  expect(firstElement.contains(mockProducts[0].name)).toEqual(true);
+});
