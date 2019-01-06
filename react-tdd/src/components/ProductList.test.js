a9c9f24c20da1a6bc5367a5f9f4ecdfde8a5dff6
djstephenson7 <dan_0502@hotmail.co.uk>
2019-01-06 17:48:45 +0000

    Testing for ProductSelect click function

--- react-tdd/src/components/ProductList.test.js
+++ react-tdd/src/components/ProductList.test.js
@@ -3,16 +3,29 @@ import ProductList from './ProductList';
 import { shallow } from 'enzyme';
 import './setupTests';
 
-let mockProducts, wrapper;
+let mockProducts, wrapper, productSelectFn;
 
 beforeEach(() => {
+
   mockProducts  = [
     { id: 1, name: 'MockProduct1', brand: 'MockBrand1' },
     { id: 1, name: 'MockProduct2', brand: 'MockBrand2' },
     { id: 1, name: 'MockProduct3', brand: 'MockBrand3' },
   ];
-  wrapper = shallow(<ProductList products={mockProducts}/>);
-});
+
+  productSelectFn = jest.fn();
+
+  wrapper = shallow(
+    <ProductList
+      products={mockProducts}
+      onProductSelect={productSelectFn}
+      />
+    );
+  });
+
+  afterEach(() => {
+    productSelectFn.mockReset();
+  })
 
 it('should render a list of components as an unordered list', () => {
   expect(wrapper.find('li').length).toEqual(mockProducts.length)
@@ -26,4 +39,20 @@ it('should display the product name in each <li> element', () => {
 it('should display the brand name in each <li> element', () => {
   const firstElement = wrapper.find('li').first();
   expect(firstElement.contains(mockProducts[0].brand)).toEqual(true);
+});
+
+it('should call props.onProductSelect when a <li> is clicked', () => {
+  const firstElement = wrapper.find('li').first();
+
+  // Check the function has not yet been called.
+  expect(productSelectFn.mock.calls.length).toEqual(0);
+
+  // Click the <li>
+  firstElement.simulate('click');
+
+  // Check the function has now been called.
+  expect(productSelectFn.mock.calls.length).toEqual(1);
+
+  // Check it's been called with the right arguments.
+  expect(productSelectFn.mock.calls[0][0]).toEqual(mockProducts[0]);
 });
