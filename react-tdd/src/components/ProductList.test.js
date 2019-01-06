f6ebc29083bd1c58cccff57be93585c3914bffae
djstephenson7 <dan_0502@hotmail.co.uk>
2019-01-06 14:50:09 +0000

    Refactored ProductList.test.js

--- react-tdd/src/components/ProductList.test.js
+++ react-tdd/src/components/ProductList.test.js
@@ -3,37 +3,27 @@ import ProductList from './ProductList';
 import { shallow } from 'enzyme';
 import './setupTests';
 
-it('should render a list of components as an unordered list', () => {
-  const mockProducts  = [
+let mockProducts, wrapper;
+
+beforeEach(() => {
+  mockProducts  = [
     { id: 1, name: 'MockProduct1', brand: 'MockBrand1' },
     { id: 1, name: 'MockProduct2', brand: 'MockBrand2' },
-    { id: 1, name: 'MockProduct3', brand: 'MockBrand3' }
-  ]
+    { id: 1, name: 'MockProduct3', brand: 'MockBrand3' },
+  ];
+  wrapper = shallow(<ProductList products={mockProducts}/>);
+});
 
-  const wrapper = shallow(<ProductList products={mockProducts}/>)
+it('should render a list of components as an unordered list', () => {
   expect(wrapper.find('li').length).toEqual(mockProducts.length)
 });
 
 it('should display the product name in each <li> element', () => {
-  const mockProducts  = [
-    { id: 1, name: 'MockProduct1', brand: 'MockBrand1' },
-    { id: 1, name: 'MockProduct2', brand: 'MockBrand2' },
-    { id: 1, name: 'MockProduct3', brand: 'MockBrand3' }
-  ]
-
-  const wrapper = shallow(<ProductList products={mockProducts}/>)
   const firstElement = wrapper.find('li').first();
   expect(firstElement.contains(mockProducts[0].name)).toEqual(true);
 });
 
 it('should display the brand name in each <li> element', () => {
-  const mockProducts  = [
-    { id: 1, name: 'MockProduct1', brand: 'MockBrand1' },
-    { id: 1, name: 'MockProduct2', brand: 'MockBrand2' },
-    { id: 1, name: 'MockProduct3', brand: 'MockBrand3' }
-  ]
-
-  const wrapper = shallow(<ProductList products={mockProducts}/>)
   const firstElement = wrapper.find('li').first();
   expect(firstElement.contains(mockProducts[0].brand)).toEqual(true);
 });
